e8d0be4e18119448a7e415a7fc8ee90b2e5e79d8
sneh-a-15 <nairsneha1508@gmail.com>
2025-07-21 23:11:48 +0530

    -

--- cs-ui/src/app/layout.tsx
+++ cs-ui/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next" 
+import { Analytics } from "@vercel/analytics/react" 
 import "./globals.css";
 
 const geistSans = Geist({
